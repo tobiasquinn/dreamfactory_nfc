e58cdec9a2304dff3a3307c1108001adf095a0a4
Tobias Quinn <tobias@tobiasquinn.com>
2013-08-25 16:19:48 +0100

    write inserts into nfc prescence database

--- nfcpresence/client/nfcpresence.js
+++ nfcpresence/client/nfcpresence.js
@@ -1,11 +1,13 @@
-Template.hello.greeting = function () {
-    return "Welcome to nfcpresence.";
+NFC = new Meteor.Collection("nfc");
+
+Template.nfcids.nfc_present = function () {
+    return NFC.find();
 };
 
-Template.hello.events({
-    'click input' : function () {
-        // template data, if any, is available in 'this'
-        if (typeof console !== 'undefined')
-    console.log("You pressed the button");
-    }
-});
+//Template.hello.events({
+//    'click input' : function () {
+//        // template data, if any, is available in 'this'
+//        if (typeof console !== 'undefined')
+//    console.log("You pressed the button");
+//    }
+//});
--- nfcpresence/server/nfcpresence.js
+++ nfcpresence/server/nfcpresence.js
@@ -1,7 +1,16 @@
+NFC = new Meteor.Collection("nfc");
+
+NFC.remove({});
+
 Meteor.Router.add({
     '/nfcinsert/:id': function(id) {
         console.log("NFC PRESENT");
         console.log("ID ", id);
+        if (NFC.find({nfcid: id}).count() === 0) {
+            NFC.insert({nfcid: id});
+        } else {
+            NFC.remove({nfcid: id});
+        }
     },
     '/nfcremoved': function() {
         console.log("NFC REMOVED");
