908140113960cf0d1a3d5f00ed4c1ab6c031d38c
Tobias Quinn <tobias@tobiasquinn.com>
2013-09-08 17:10:18 +0100

    added command line arg for nfc number

--- testing/insertnfcs.sh
+++ testing/insertnfcs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl http://localhost:3000/nfcinsert/test1
+curl http://localhost:3000/nfcinsert/test$1
 #curl http://localhost:3000/nfcinsert/test2
 #curl http://localhost:3000/nfcinsert/test3
 #curl http://localhost:3000/nfcinsert/test4
