8e5044e41a00482ea30a2ce190ed29dad9b36698
Tobias Quinn <tobias@tobiasquinn.com>
2013-09-08 18:23:57 +0100

    only show mapped entries in nfcmap

--- nfcpresence/client/nfcmap.js
+++ nfcpresence/client/nfcmap.js
@@ -1,3 +1,7 @@
 Template.nfcmap.nfcmap = function() {
     return NFCMAP.find({}).fetch();
 }
+
+Template.nfcmap.nfcmapassigned = function() {
+    return NFCMAP.find({nfcid: {$ne: undefined}}).fetch();
+}
