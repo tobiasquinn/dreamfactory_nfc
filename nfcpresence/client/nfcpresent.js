b529abb4e99a779b75f5ad249ddaaf557ecc7172
Tobias Quinn <tobias@tobiasquinn.com>
2013-09-09 03:04:32 +0100

    added date field to nfc present output

--- nfcpresence/client/nfcpresent.js
+++ nfcpresence/client/nfcpresent.js
@@ -9,5 +9,10 @@ Template.nfcpresent.nfc_present = function () {
     // we have the map of present tags, we want the matrix records for the rows
     var maprows = _.map(map, function(value) { return value.rowNumber });
     var records = MATRIX.find({rowNumber: {$in: maprows}}).fetch();
+    _.each(records, function(record) {
+        var nfcid = NFCMAP.findOne({rowNumber: record.rowNumber}).nfcid;
+        var date = NFC.findOne({nfcid: nfcid}).date;
+        record.date = date;
+    });
     return records;
 };
