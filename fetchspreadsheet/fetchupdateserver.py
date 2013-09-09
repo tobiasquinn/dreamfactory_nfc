9eeb38e1012597d4f0528a61709ec35a2465ed04
Tobias Quinn <tobias@tobiasquinn.com>
2013-09-09 22:17:02 +0100

    fetch debug and button change

--- fetchspreadsheet/fetchupdateserver.py
+++ fetchspreadsheet/fetchupdateserver.py
@@ -4,7 +4,9 @@ import os
 
 @route('/9yI04CuSx6NQ1Lf8am7m')
 def updatesheets():
+    print("Start fetch")
     os.system("node fetchmatrix.js")
+    print("End fetch")
     return "Updated From Matrix Spreadsheet"
 
 run(host="0.0.0.0", port=9453)
