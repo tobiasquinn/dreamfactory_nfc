9dc276267164bf7bc34446ec08839b617ec499d3
Tobias Quinn <tobias@tobiasquinn.com>
2013-09-09 01:47:15 +0100

    nicer bootstrap theme, fetch server and landing page

--- fetchspreadsheet/fetchupdateserver.py
+++ fetchspreadsheet/fetchupdateserver.py
@@ -2,9 +2,9 @@
 from bottle import route, run
 import os
 
-@route('/nd9GMXhXWWOieoY1yCiH')
+@route('/9yI04CuSx6NQ1Lf8am7m')
 def updatesheets():
-    os.system("node fetchlessons.js")
-    return "Updated Spreadsheet"
+    os.system("node fetchmatrix.js")
+    return "Updated From Matrix Spreadsheet"
 
 run(host="0.0.0.0", port=9453)
