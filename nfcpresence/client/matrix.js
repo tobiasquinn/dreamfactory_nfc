22c1f9c6cfcb07b345248730070c2c40e2078024
Tobias Quinn <tobias@tobiasquinn.com>
2013-09-09 15:31:19 +0100

    added start of matrix refresh url

--- nfcpresence/client/matrix.js
+++ nfcpresence/client/matrix.js
@@ -1,5 +1,7 @@
 MATRIX = new Meteor.Collection("matrix");
 
+var REFRESH_URL = "http://matrix.thedreamfactory.net:9453/9yI04CuSx6NQ1Lf8am7m";
+
 Meteor.startup(function() {
     Session.set('searchfilter', '');
 });
@@ -13,11 +15,21 @@ Template.matrix.entry = function() {
     return MATRIX.find({name: { $regex: Session.get('searchfilter'), $options: 'i'}}, {sort: {name: 1}});
 }
 
+Template.matrix.refreshResult = function() {
+    return Session.get("refreshResult");
+}
+
 Template.matrix.events = {
     'keyup #searchfilter': function(evt) {
         Session.set('searchfilter', evt.target.value);
     },
     'click #clear': function() {
         Session.set('searchfilter', undefined);
+    },
+    'click #refreshMatrix': function() {
+        $.get(REFRESH_URL, function(result) {
+            console.log("REFRSH", result);
+            Session.set('refreshResult', result);
+        });
     },
 }
